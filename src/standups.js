5da038f8b50860ec3862e75b22959e5434e8b8cb
Daniël Illouz <daniel.illouz@me.com>
2019-10-18 14:03:48 +0200

    Only fetch updates of standups a user is member of
    Fixes #14
    
    Unfortunately DynamoDB does not support Query for Transaction operations.
    Therefore we have to write a separate routine to fetch the standup/user
    item.

--- src/standups.js
+++ src/standups.js
@@ -164,11 +164,39 @@ module.exports = {
         if (!userItem.Item) {
           const err = new Error('Not Found');
           err.statusCode = 404;
-          err.details = 'you might not be a member of this standup';
+          err.details = 'You might not be a member of this standup.';
           throw err;
         }
 
         return standupItem.Item;
       });
+  },
+
+  /**
+   * Check if a user is a member of a standup.
+   *
+   * @param {Object} client - DynamoDB document client
+   * @param {String} tableName - DynamoDB table name
+   * @param {String} standupId
+   * @param {String} userId
+   *
+   * @return {Promise} Resolves with Boolean
+   *
+   * For SDK documentation see:
+   * https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#get-property
+   */
+  userIsMember(client, tableName, standupId, userId) {
+    const params = {
+      TableName: tableName,
+      Key: {
+        pk: `standup#${standupId}`,
+        sk: `user#${userId}`
+      }
+    };
+
+    return client
+      .get(params)
+      .promise()
+      .then(data => Boolean(data.Item));
   }
 };
